fa37ceb8782a60e16cdc0bffdfd66403ba5a45e9
Arnel Jan Sarmiento <aesarmiento@up.edu.ph>
2022-10-12 19:51:41 +0800

    File Structure initialization

---
+++ Hugo_Paderna_Sarmiento_02/Hugo_Paderna_Sarmiento_02.cpp
@@ -0,0 +1,21 @@
+// for standard I/O
+#include <iostream>
+using namespace std;
+
+// for string class
+#include <string>
+
+int main()
+{
+    cout << "\nList MENU"
+         << "\n\t[1]Add Item"
+         << "\n\t[2]Delete Item"
+         << "\n\t[3]Display List"
+         << "\n\t[4]Delete List"
+         << "\n\t[5]Search Item"
+         << "\n\t[0]Exit Program"
+         << "\nEnter choice: "
+         << endl;
+
+    return 0;
+}
