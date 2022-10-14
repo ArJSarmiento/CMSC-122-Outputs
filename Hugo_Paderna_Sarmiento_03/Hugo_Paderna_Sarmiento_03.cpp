0f44647ca2121b980fa767d2a5f92c92aa49e45c
Arnel Jan Sarmiento <aesarmiento@up.edu.ph>
2022-10-14 18:39:31 +0800

    Stack problem set starter function

--- Hugo_Paderna_Sarmiento_03/Hugo_Paderna_Sarmiento_03.cpp
+++ Hugo_Paderna_Sarmiento_03/Hugo_Paderna_Sarmiento_03.cpp
@@ -1,27 +1,102 @@
-// for standard I/O
-#include <iostream>
-using namespace std;
+#include "Stack.cpp"
 
-// for string class
-#include <string>
+// function prototype declaration
+Stack *PUSH(Stack *stack, string kw);
+Stack *POP(Stack *stack);
+Stack *MAKENULL(Stack *stack);
+void TOP(Stack *stack);
 
 // displays the program summary
 void summary()
 {
-    cout << "\nList MENU"
-         << "\n\t[1]Add Item"
-         << "\n\t[2]Delete Item"
-         << "\n\t[3]Display List"
-         << "\n\t[4]Delete List"
-         << "\n\t[5]Search Item"
-         << "\n\t[0]Exit Program"
-         << "\nEnter choice: "
-         << endl;
+    cout <<"\nKitchenware Washing Machine"
+        <<"\n\t[1] Add kitchenware"
+        <<"\n\t[2] Wash kitchenware"
+        <<"\n\t[3] Top kitchenware"
+        <<"\n\t[4] Wash all"
+        <<"\n\t[0] Exit"
+        <<"\nEnter choice: "
+        << endl;
 }
 
 int main()
 {
-    summary(); 
+    Stack *stack = new Stack(); 
+
+    int choice, value;
+    string kitchenware;
+
+    for (choice = 6; choice != 0;)
+    {
+        summary();
+
+        cin >> choice;
+
+        switch (choice) // use switch flow control structure
+        {
+        case 1:
+            cout << "\nAdd kitchenware (ex. Plate, Bowl, etc.) to be washed: ";
+            cin >> kitchenware;
+            stack = PUSH(stack, kitchenware);
+            break; // this breaks from the switch(), not from the loop
+        case 2:
+            // stack = POP(stack);
+            break;
+        case 3:
+            TOP(stack);
+            break;
+        case 4:
+            // stack = MAKENULL(stack);
+            break;
+        case 0:
+            cout << "\nThe Washing Machine process has been terminated..."; // colorPrint adds color to the text; 12 corresponds to red
+        }
+    }
 
     return 0;
+}
+
+bool FULL(Stack *stack)
+{
+    return stack->top >= stack->max;
+}
+
+bool EMPTY(Stack *stack)
+{
+    return stack->top == -1;
+}
+
+Stack *PUSH(Stack *stack, string kw)
+{
+    if (FULL(stack))
+    {
+        cout << "\nThe STACK is FULL. Cannot add kitchenware.";
+        return stack;
+    }
+
+    stack->top++;
+    stack->data[stack->top] = kw;
+    cout << "\n" << kw << " has been added to the stack\n";
+    return stack;
+}
+
+void TOP(Stack *stack)
+{
+    if (EMPTY(stack))
+    {
+        cout << "\nThe STACK is EMPTY. No kitchenware to wash.";
+        return;
+    }
+
+    cout << "\n" << stack->data[stack->top] << " is next to be washed.\n";
+}
+
+Stack *POP(Stack *stack)
+{
+    return stack;
+}
+
+Stack *MAKENULL(Stack *stack)
+{
+    return stack;
 }
---
+++ Hugo_Paderna_Sarmiento_03/Stack.cpp
@@ -0,0 +1,12 @@
+// Type Node definition
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Stack
+{
+public:
+    int max = 100;
+    string data[100];
+    int top = -1;
+};
